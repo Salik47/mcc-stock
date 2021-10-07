6be64200e0a9607431eb181cfab3747aa50cf127
unknown <msalik32@gmail.com>
2021-10-07 14:29:27 +0530

    App working

---
+++ App.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Alert, View } from 'react-native';
+import BottomNavigator from './navigation/BottomNavigator';
+import { DarkTheme, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AppearanceProvider } from 'react-native-appearance';
+import * as Location from 'expo-location';
+import * as MediaLibrary from 'expo-media-library';
+import AuthScreen from './screens/AuthScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const requestPermissions = async () => {
+    console.log('Requesting permissions...');
+    const { status: mls } = await MediaLibrary.requestPermissionsAsync();
+    const { status: ls } = await Location.requestForegroundPermissionsAsync();
+
+    if ([mls, ls].includes('denied')) {
+      console.log('Permissions not granted');
+      Alert.alert(
+        'Permissions required',
+        'Please enable permissions for the app to work properly'
+      );
+      return;
+    }
+
+    console.log('Permissions granted');
+  };
+
+  useEffect(() => {
+    requestPermissions();
+  }, []);
+
+  return (
+    <AppearanceProvider>
+      <View style={{ flex: 1 }}>
+        <StatusBar style="auto" />
+        <NavigationContainer theme={DarkTheme}>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="auth"
+              component={AuthScreen}
+            />
+            <Stack.Screen name="mcc-stock" component={BottomNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </AppearanceProvider>
+  );
+}
